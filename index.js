083a4c8276ec3b0469b54a9e1e505606047b2c6f
Alvin jangvik <ajangvik@gmail.com>
2025-07-23 17:50:12 +0200

    Actual project
    simple docker website on port 9000

---
+++ index.js
@@ -0,0 +1,12 @@
+import express from 'express';
+
+const app = express();
+
+app.get('/', (req, res) => {
+	res.send("Welcome person");
+});
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+	console.log('Unfortunately listening on port http://localhost${port}');
+});
