91128a423f2363f2b6a4c954acff09b945103c6a
Alvin jangvik <ajangvik@gmail.com>
2025-07-24 17:20:23 +0200

    Removed test files
    kept index_old to try and convert it to ts

--- index.js
+++ index.js
@@ -1,4 +1,32 @@
+var Car = /** @class */ (function () {
+    function Car() {
+    }
+    Car.prototype.consructor = function (t, m, y) {
+        this.type = t;
+        this.model = m;
+        this.year = y;
+    };
+    return Car;
+}());
+var numbers = [1, 2, 3];
+numbers.push(4);
+var car = new Car("Toyota", "Corolla", 2009);
 function greet(name) {
     return "Hello, ".concat(name, "! Welcome to TypeScript.");
 }
+function printNumbers(a) {
+    for (var i = 0; i < a.length; i++) {
+        console.log("Number ".concat(a[i]));
+    }
+}
+function printCar(c) {
+    if (c instanceof Car) {
+        console.log("This is a car");
+    }
+    console.log("Brand: ".concat(c.type));
+    console.log("Model: ".concat(c.model));
+    console.log("Year: ".concat(c.year));
+}
 console.log(greet("Developer"));
+printNumbers(numbers);
+printCar(car);
--- index.ts
+++ index.ts
@@ -1,5 +1,40 @@
+class Car { 
+	type: string; 
+	model: string; 
+	year: number;
+
+	consructor(t: string, m: string, y: number) {
+		this.type = t;
+		this.model = m;
+		this.year = y;
+	}
+ }
+
+const numbers = [1, 2, 3];
+numbers.push(4);
+
+const car = new Car("Toyota", "Corolla", 2009);
+
 function greet(name: string): string {
 	return `Hello, ${name}! Welcome to TypeScript.`;
 }
 
-console.log(greet("Developer"))
+function printNumbers(a) {
+	for (let i = 0; i < a.length; i++) {
+		console.log(`Number ${a[i]}`)
+	}
+}
+
+function printCar(c: Car) {
+	if(c instanceof Car){
+		console.log("This is a car")
+	}
+
+	console.log(`Brand: ${c.type}`);
+	console.log(`Model: ${c.model}`);
+	console.log(`Year: ${c.year}`);
+}
+
+console.log(greet("Developer"));
+printNumbers(numbers);
+printCar(car);
