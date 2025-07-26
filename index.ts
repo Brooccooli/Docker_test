fa634eb929c1abfa3fc828d43cf879e89f3fee6a
Alvin jangvik <ajangvik@gmail.com>
2025-07-26 15:37:51 +0200

    wrong brace

--- index.ts
+++ index.ts
@@ -8,5 +8,5 @@ app.get('/', (req, res) => {
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
-	console.log('Listening on port http://localhost${port}');
+	console.log(`Listening on port http://localhost${port}`);
 });
