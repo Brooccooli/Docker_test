59642b0ed176830f6e089b00d283e95f4ad19d5f
Alvin jangvik <ajangvik@gmail.com>
2025-07-27 21:26:44 +0200

    Added pug to get a cool website

--- index.ts
+++ index.ts
@@ -2,8 +2,11 @@ import express from 'express';
 
 const app = express();
 
+app.set('view engine', 'pug');
+
 app.get('/', (req, res) => {
-	res.send("Welcome person");
+	// res.send("Welcome person");
+	res.render('index');
 });
 
 const port = process.env.PORT || 3000;
